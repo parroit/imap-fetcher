ba9fdac5428b24c364d796a3610772583ddbe481
parroit <andrea.parodi@ebansoftware.net>
2013-10-12 18:22:40 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# imapFetcher [![Build Status](https://secure.travis-ci.org/parroit/imapFetcher.png?branch=master)](http://travis-ci.org/parroit/imapFetcher) [![Npm module](https://badge.fury.io/js/imapFetcher.png)](https://npmjs.org/package/imapFetcher)
+
+Fetches mail messages from imap and publish them to [corriera event bus](https://github.com/parroit/corriera)
+
+## Getting Started
+Install the module with: `npm install imapFetcher`
+
+```javascript
+var imapFetcher = require('imapFetcher');
+imapFetcher.awesome(); // "awesome"
+```
+
+## Test
+To test the package, configure run and then grunt test.
+Please note the mail access configuration make use of
+environment for security reasons.
+
+Before to run tests, you have to set environment variables
+MY_MAIL_ADDRESS and MY_MAIL_PASSWORD to the user and password
+of the mail account you want to use.
+
+## Documentation
+_(Coming soon)_
+
+## Examples
+_(Coming soon)_
+
+## Contributing
+In lieu of a formal styleguide, take care to maintain the existing coding style. Add unit tests for any new or changed functionality. Lint and test your code using [Grunt](http://gruntjs.com/).
+
+## Release History
+_(Nothing yet)_
+
+## License
+Copyright (c) 2013 Andrea Parodi  
+Licensed under the MIT license.
---
+++ test/imapFetcher_test.js
@@ -0,0 +1,123 @@
+/*
+ * imapFetcher
+ * https://github.com/parroit/imapFetcher
+ *
+ * Copyright (c) 2013 Andrea Parodi
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+var imap = require('../lib/imapFetcher');
+
+var Fetcher = imap.Fetcher;
+
+var expect = require('chai').expect;
+require('chai').should();
+
+
+describe('imapFetcher',function(){
+    describe("module",function() {
+        it("should load",function(){
+            expect(imap).not.to.be.equal(null);
+            expect(imap).to.be.a('object');
+
+        })
+    });
+
+    describe("Fetcher",function() {
+        var transport = new Fetcher({
+            user: process.env.MY_MAIL_ADDRESS,
+            password: process.env.MY_MAIL_PASSWORD,
+            host: 'imap.gmail.com',
+            port: 993,
+            tls: true,
+            tlsOptions: { rejectUnauthorized: false }
+        });
+
+        it("should be instantiable with config",function(){
+            expect(transport).not.to.be.null;
+        });
+        describe("status",function(){
+
+
+            it("should be disconnected at start",function(){
+                expect(transport.status).to.be.equal(imap.Status.disconnected);
+
+            });
+
+            it("go to connected upon connection",function(done){
+                this.timeout(5000);
+                transport.connect(function(){
+                    expect(transport.status).to.be.equal(imap.Status.connected);
+                    transport.disconnect(function(){
+                        done();
+                    });
+
+                });
+
+
+            });
+
+            it("go to disconnected upon disconnection",function(done){
+                this.timeout(5000);
+                transport.connect(function(){
+
+                    transport.disconnect(function(){
+                        expect(transport.status).to.be.equal(imap.Status.disconnected);
+                        done();
+                    });
+                });
+
+
+            });
+
+        });
+        describe("fetch",function(){
+
+
+            it("broadcast events on message received",function(done){
+                this.timeout(25000);
+                var bus = require("corriera");
+                bus.once('messageFetched',/.*/,function(messageStream,messageInfo){
+                    expect(messageStream).to.be.not.null;
+                    transport.disconnect(function(){
+                        done();
+                    });
+
+                });
+                transport.connect(function(){
+                    transport.fetch("INBOX",'1:1');
+                });
+
+
+            });
+
+            it("broadcasted message stream contains full message body",function(done){
+                this.timeout(25000);
+                var bus = require("corriera");
+                bus.once('messageFetched',/.*/,function(messageStream,messageInfo){
+                    var buffer = '';
+                    messageStream.on('data', function(chunk) {
+                        buffer += chunk.toString('utf8');
+                    });
+                    messageStream.once('end', function() {
+                        var fs = require("fs");
+                        var expected = fs.readFileSync("./test/expected.msg","utf8");
+                        expect(buffer).to.be.equal(expected);
+                        transport.disconnect(function(){
+                            done();
+                        });
+                    });
+
+
+                });
+                transport.connect(function(){
+                    transport.fetch("INBOX/test",'1:1');
+                });
+
+
+            });
+        });
+    });
+});
